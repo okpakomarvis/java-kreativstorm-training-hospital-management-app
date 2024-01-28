9fa6932e0c40f006ed1106292e1c93d84dedb712
Jan Hršak (jhrsak) <jhrsak@tvz.hr>
2024-01-28 11:36:12 +0100

    Initial commit

---
+++ src/main/java/com/kreativstorm/hms/entities/Nurse.java
@@ -0,0 +1,12 @@
+package com.kreativstorm.hms.entities;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class Nurse {
+    String username, name, email, phone;
+}
