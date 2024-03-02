b74b6c0b89930b1448253ac76e9336f07aa1f722
Jan Hršak (jhrsak) <jhrsak@tvz.hr>
2024-03-02 12:09:33 +0100

    added method to get all appointments

--- src/main/java/com/kreativstorm/hms/service/impl/AppointmentServiceImpl.java
+++ src/main/java/com/kreativstorm/hms/service/impl/AppointmentServiceImpl.java
@@ -14,6 +14,7 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import java.util.List;
 import java.util.Optional;
 
 @Service
@@ -50,5 +51,10 @@ public class AppointmentServiceImpl implements AppointmentService {
         appointment1.setPatientID(appointment.getPatientID());
         appointment1.setScheduledFor(appointment.getScheduledFor());
         return Optional.of(appointmentRepository.saveAndFlush(appointment1));
+    }
+
+    @Override
+    public List<Appointment> getAll() {
+        return appointmentRepository.findAll();
     }
 }
